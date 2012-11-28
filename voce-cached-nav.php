e54b5422059d51e0c8c8608e2519fefe8ff6885d
Mark Parolisi <mark@voceconnect.com>
2012-11-28 09:49:19 -0500

    checking if nav menu locations exist

--- voce-cached-nav.php
+++ voce-cached-nav.php
@@ -128,6 +128,9 @@ if ( !class_exists( 'Voce_Cached_Nav' ) ) {
 		public static function get_nav_menu_object( $args ) {
 			if ( empty( $args->menu ) ) {
 				$locations = get_nav_menu_locations();
+				if ( empty( $locations ) ) {
+					return false;
+				}
 				$menu_lookup = $locations[$args->theme_location];
 			} else {
 				$menu_lookup = $args->menu;
